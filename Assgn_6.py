8e8d1193a0de39b6f0b9ab471e2d4fbce2ceda66
RoshanatNiser <roshan.yadav@niser.ac.in>
2025-09-03 18:00:09 +0530

    Update Assgn_6.py

--- Assgn_6.py
+++ Assgn_6.py
@@ -5,9 +5,9 @@
 from Func_lib_for_assgn_6 import *
 
 #Question-1: Finding Roots
-print('Root by Bisection method', Bisection(a=1.5,b=3.0,f=0)) 
-print('\nRoot by Regular Falsi method', regular_falsi(a=1.5,b=3.0,c=0,f=0))
+print('Root by Bisection method', Bisection(a=1.5,b=3.0,t=0)) # Output: 2.623140335083008
+print('\nRoot by Regular Falsi method', regular_falsi(a=1.5,b=3.0,t=0)) #Output: 2.6231403354363083
 
 #Question-2: Bracketing
-print('\nBracketing of f(x)=-x-cos(x)',bracketing(a=2,b=4,f=0,beta=0.2))
+print('\nBracketing of f(x)=-x-cos(x)',bracketing(a=2,b=4,t=0,beta=0.2)) # output: [0.40000000000000013, 4]
 
